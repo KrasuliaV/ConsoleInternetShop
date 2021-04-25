e2f96abe593144d5dfd4be4a2ffe3020e4df870d
KrasuliaV <Fenix2104>
2021-04-25 20:11:16 +0300

    Created ProductDaoImpl

--- src/main/java/dao/ProductDao.java
+++ src/main/java/dao/ProductDao.java
@@ -3,4 +3,7 @@ package dao;
 import model.Product;
 
 public interface ProductDao extends Dao<Product> {
+
+    Product getByProductName(String productName);
+
 }
---
+++ src/main/java/dao/impl/ProductDaoImpl.java
@@ -0,0 +1,52 @@
+package dao.impl;
+
+import dao.ProductDao;
+import db.HomeDB;
+import model.Product;
+
+import java.util.List;
+
+public class ProductDaoImpl implements ProductDao {
+
+    private static long productId = 3;
+
+    @Override
+    public Product create(Product product) {
+        productId++;
+        product.setId(productId);
+        HomeDB.getProductDB().add(product);
+        return product;
+    }
+
+    @Override
+    public Product getById(Long id) {
+        return HomeDB.getProductDB().stream()
+                .filter(product -> product.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public Product delete(Product product) {
+        HomeDB.getProductDB().remove(product);
+        return product;
+    }
+
+    @Override
+    public Product update(Product product) {
+        return HomeDB.getProductDB().set(HomeDB.getProductDB().indexOf(getById(product.getId())), product);
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return HomeDB.getProductDB();
+    }
+
+    @Override
+    public Product getByProductName(String productName) {
+        return HomeDB.getProductDB().stream()
+                .filter(product -> product.getName().equals(productName))
+                .findFirst()
+                .orElse(null);
+    }
+}
