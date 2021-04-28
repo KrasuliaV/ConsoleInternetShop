1a5ab7df95d5b16b0af943f34760dd59dade6331
KrasuliaV <Fenix2104>
2021-04-28 16:02:58 +0300

    Added some style change

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -1,13 +1,15 @@
 package view;
 
+import view.style.ConsoleColors;
+
 public interface Menu {
 
     void exit();
 
     default void showItems(String[] items) {
-        System.out.println("Choose operation(enter operation's number)");
+        System.out.printf("%sChoose operation(enter operation's number)%s%n", ConsoleColors.YELLOW_BOLD.getColorCode(), ConsoleColors.RESET.getColorCode());
         for (String item : items) {
-            System.out.printf("%-25s", item);
+            System.out.printf("%s%-25s%s", ConsoleColors.GREEN_BOLD_BRIGHT.getColorCode(), item, ConsoleColors.RESET.getColorCode());
         }
         System.out.println("");
     }
